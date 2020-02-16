3b5051b77c2465d419cf9d9891150fbee2c92f61
David Lau <David.lau@codeimmersives.com>
2020-02-16 18:07:24 -0500

    done

--- README.md
+++ README.md
@@ -1,4 +1,89 @@
-# While You Were Coding
-### Some While Loop Challenges
+// ##### Introduction #####
+//
+// The below assignment will require NO functions, except for the challenge banner function in number 1.
+//
+// Instead, our focus is going to be on console.logging the characters at certain indices of strings.
+//
 
-All the directions are in `main.js`. Enjoy!
+
+
+// ##### Challenges #####
+
+// 1.
+//
+// Write a function that takes in a parameter and prints the string below, with the given paramter place of the $ symbol.
+// 
+// ###### Challenge $ #####
+//
+// Call it before EVERY challenge below!
+//
+// Even though we'll USUALLY be calling this function with a number indicating the challenge we're on, THIS TIME call it with the parameter 'Banner Function Works!'. You should see the below result:
+//
+// ###### Challenge Banner Function Works! #####
+
+
+// 2.
+//
+// Console out the numbers from 1-20, but only if they're divisible by 3.
+
+// Use `if` and a modulus operation.
+
+
+// 3.
+//
+// Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
+
+
+
+// 4.
+//
+// Console out the numbers from 1-20. BACKWARDS.
+
+
+// 5.
+//
+// For the string 'Boy howdy am I good at this!', console out the letters
+// individually.
+
+
+// 6.
+//
+// For the string, 'And getting better every day.', starting with the second
+// character, print out every third character.
+
+
+// 7. 
+//
+// For the string 'Am I the best?', console out the letters individually, AND
+// UPPERCASED.
+
+
+// 8.
+//
+// For the string 'Whoa, I am the best!', console out the letters individually,
+// but also capitalize the letters in the third word.
+
+
+// 9.
+//
+// For the string 'I am become death, destroyer of worlds.', console out the letters
+//  individually, but backwards. That is, the first console log should be
+// `.`, followed by `s`, followed by `d`, and so on.
+
+
+// 10.
+//
+const names = 'Colin|Mesuara|Denis|Wilson|Michael|Patrick|DeAundre|Jumary|Marc|Bogdan|Sharod|David|LaToddra|Carlos';
+
+// Console out the individual letters of each person in class today, which are
+// in the string above. Every time you start a new name (including the first
+// one!), console out the string: 'Here today is:'.
+
+// You'll know you're at a new person's name when you hit the pipe character.
+
+// Don't print the pipes!
+
+
+// 11.
+//
+// There is no 11. Celebrate! You did it!
--- main.js
+++ main.js
@@ -1,89 +1,87 @@
-// ##### Introduction #####
-//
-// The below assignment will require NO functions, except for the challenge banner function in number 1.
-//
-// Instead, our focus is going to be on console.logging the characters at certain indices of strings.
-//
+banner = (x) => {
+     console.log(`##### Challenges ${x}#####`)
+}
+
+banner(2)
+
+let cTwo = 0
+while (cTwo < 20) {
+    cTwo ++
+    if (cTwo%3===0) console.log(cTwo)
+}
+
+// banner(3 )
+// let divBy3 = 0
+// while (divBy3 < 100){
+//     divBy3 = divBy3 + 3
+// }
+
+// let cThree = 0
+// while (cThree < 20) {
+//     cThree ++
+//     (cThree === divBy3) 
+// }
+
+banner (4)
 
 
+for (let i = 20 ; i > 0; i-- ){
+    console.log(i)
+}
 
-// ##### Challenges #####
+banner (5)
+let str = 'Boy howdy am I good at this!'
+for (let i = 0 ; i < str.length; i++){
+    console.log(str[i])
+}
 
-// 1.
-//
-// Write a function that takes in a parameter and prints the string below, with the given paramter place of the $ symbol.
-// 
-// ###### Challenge $ #####
-//
-// Call it before EVERY challenge below!
-//
-// Even though we'll USUALLY be calling this function with a number indicating the challenge we're on, THIS TIME call it with the parameter 'Banner Function Works!'. You should see the below result:
-//
-// ###### Challenge Banner Function Works! #####
+banner (6)
 
+let str2 = 'And getting better every day.'
+for (let i = 0 ; i < str2.length; i++){
+   if (i%3 === 0) console.log(str2[i +1])
+}
 
-// 2.
-//
-// Console out the numbers from 1-20, but only if they're divisible by 3.
+banner (7)
 
-// Use `if` and a modulus operation.
+const str3 = "A'm I the best?"
+for (let i = 0 ; i < str3.length; i++) {
+    console.log(str3[i].toUpperCase())
+}
 
+banner (8)
 
-// 3.
-//
-// Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
-
-
-
-// 4.
-//
-// Console out the numbers from 1-20. BACKWARDS.
-
-
-// 5.
-//
-// For the string 'Boy howdy am I good at this!', console out the letters
-// individually.
+const str4 = 'Whoa, I am the best!'
+let newfour = 0
+for (let i = 0 ; i < str4.length; i++) {
+    if (str4[i] ===' ') newfour = newfour + 1 
+        if (newfour=== 2)
+        console.log(str4[i].toUpperCase())
+        else 
+            console.log(str4[i])
+    }
 
 
-// 6.
-//
-// For the string, 'And getting better every day.', starting with the second
-// character, print out every third character.
+    
 
 
-// 7. 
-//
-// For the string 'Am I the best?', console out the letters individually, AND
-// UPPERCASED.
+banner (9)
+
+const str5 = 'I am become death, destroyer of worlds.'
+for (let i = str5.length ; i > 0 ; i = i - 1 ) {
+                console.log(str5[i])
+}
+
+banner(10)
 
 
-// 8.
-//
-// For the string 'Whoa, I am the best!', console out the letters individually,
-// but also capitalize the letters in the third word.
-
-
-// 9.
-//
-// For the string 'I am become death, destroyer of worlds.', console out the letters
-//  individually, but backwards. That is, the first console log should be
-// `.`, followed by `s`, followed by `d`, and so on.
-
-
-// 10.
-//
 const names = 'Colin|Mesuara|Denis|Wilson|Michael|Patrick|DeAundre|Jumary|Marc|Bogdan|Sharod|David|LaToddra|Carlos';
-
-// Console out the individual letters of each person in class today, which are
-// in the string above. Every time you start a new name (including the first
-// one!), console out the string: 'Here today is:'.
-
-// You'll know you're at a new person's name when you hit the pipe character.
-
-// Don't print the pipes!
-
-
-// 11.
-//
-// There is no 11. Celebrate! You did it!
+let newn = 0
+console.log('Here today is:')
+ for (let i = 0 ; i < names.length ; i++) {
+    
+    if (names[i] === '|') newn = newn + 1
+    if (names[i] === '|') console.log('Here today is:')
+    if (newn === newn && names[i] !== '|' ) console.log(names[i])
+     
+ }
